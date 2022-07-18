a9230e0a3145d42cb6dc81bebfd89426fbb1e8b2
DaniloAlves <danilobernardino786@gmail.com>
2022-07-18 20:49:28 -0300

    XP

--- js/blocos.js
+++ js/blocos.js
@@ -3,7 +3,7 @@ function card1(list, tipo) {
     if(list.length > 0){
         for(var i = 0; i < list.length; i++) {
             console.log(list.length)
-            if(list[i][3] == tipo){
+            if(list[i][3] == tipo || list[i][3] == 2){
                 itens.innerHTML += `
                     <div class="bloco1 text-center bg-dark pt-3">
                         <h1 style="color: rgb(236, 0, 236)">${list[i][0]}</h1>
@@ -11,6 +11,10 @@ function card1(list, tipo) {
                         <h4 class="text-light">${list[i][1]}</h4>
                         <hr size="1">
                         <h5 class="text-secondary">"${list[i][2]}"</h5>
+                        <div class="d-flex flex-row text-center justify-content-center text-light">
+                            <h5>XP</h5>
+                            <progress value="${list[i][3]}" max="100" class="m-2">${list[i][3]}%</progress>
+                        </div>
                     </div>
                 `
             }
@@ -20,4 +24,11 @@ function card1(list, tipo) {
         console.log("Sem itens na lista")
         itens.innerHTML = `<h2>Parece que ainda não temos esse conteúdo!</h2>`
     }
+}
+
+
+function perfil1(lista, posicao){
+
+
+
 }
---
+++ js/list.js
@@ -0,0 +1,17 @@
+/*
+
+    Tipos:
+        0 - follow;
+        1 - sub;
+        2 - ambos;
+
+*/
+
+//["Nome", "Título", "Mesagem.", tipo , experiência]
+
+lista = [
+
+    //["lAcsen_", "Mod e amigo", "Espero poder divertir e melhorar o dia de todos os que tirarem um pouco do seu tempo para participar desse sonho.", 0, 1]
+    ["Zegfy", "Criador da comunidade", "Espero poder divertir e melhorar o dia de todos os que tirarem um pouco do seu tempo para participar desse sonho.", 2, 1],
+
+];
