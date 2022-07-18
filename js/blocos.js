8c46a3dff8b3927fb4db5cd9827a909c326c83d6
DaniloAlves <danilobernardino786@gmail.com>
2022-07-18 19:56:09 -0300

    meucomentario

--- js/blocos.js
+++ js/blocos.js
@@ -13,11 +13,11 @@ function card1(list, tipo) {
             if(list[i][3] == tipo || tipo == 0){
                 itens.innerHTML += `
                     <div class="bloco1 text-center bg-dark pt-3">
-                        <h1 style="color: rgb(236, 0, 236)">Zegfy</h1>
+                        <h1 style="color: rgb(236, 0, 236)">${list[i][0]}</h1>
                         <hr size="1">
-                        <h4 class="text-light">Interagiu com 2.450 mensagens em uma live.</h4>
+                        <h4 class="text-light">${list[i][1]}</h4>
                         <hr size="1">
-                        <h5 class="text-secondary">"Me sinto parte da comunidade! Espero que várias pessoas possam ver isso no futuro."</h5>
+                        <h5 class="text-secondary">"${list[i][2]}"</h5>
                     </div>
                 `
             }
--- js/follows.js
+++ js/follows.js
@@ -1,5 +1,5 @@
 follows = [
 
-    
+    ["Zegfy", "Criador da comunidade", "Espero poder divertir e melhorar o dia de todos os que tirarem um pouco do seu tempo para participar desse sonho."]
 
 ];
