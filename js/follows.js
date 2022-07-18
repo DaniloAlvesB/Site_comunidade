1277992d838cf91f8b7dd68aa08aa896f02cdfa8
DaniloAlves <danilobernardino786@gmail.com>
2022-07-18 20:06:13 -0300

    animafollow

--- js/follows.js
+++ js/follows.js
@@ -1,5 +1,6 @@
 follows = [
 
-    ["Zegfy", "Criador da comunidade", "Espero poder divertir e melhorar o dia de todos os que tirarem um pouco do seu tempo para participar desse sonho."]
+    ["Zegfy", "Criador da comunidade", "Espero poder divertir e melhorar o dia de todos os que tirarem um pouco do seu tempo para participar desse sonho."],
+    ["lAcsen_", "Mod e amigo", "Espero poder divertir e melhorar o dia de todos os que tirarem um pouco do seu tempo para participar desse sonho."]
 
 ];
