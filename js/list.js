ee5915921e5ad47f8f46954ee19106f202d7ad02
DaniloAlves <danilobernardino786@gmail.com>
2022-07-19 10:46:51 -0300

    bigpandaria

--- js/list.js
+++ js/list.js
@@ -12,6 +12,7 @@
 lista = [
 
     //["lAcsen_", "Mod e amigo", "Espero poder divertir e melhorar o dia de todos os que tirarem um pouco do seu tempo para participar desse sonho.", 0, 1]
+    ["bigpandaria", "Primordial", "É como diz um velho conhecido ditado: Quem ri por último come mais chocolate... O que tem mais saliva ri melhor... O que ri comendo chocolate é o último que come... É quase isso aí.", 0, 1],
     ["giulialaiza", "Primordial", "Aquilo que se faz por amor está sempre além do bem e do mal. ~Friedrich Nietzsche", 0, 1],
     ["WeirdCory", "Primordial", "Sábio é aquele que conhece os limites da própria ignorância. ~Sócrates", 0, 1],
     ["Zegfy", "Criador da comunidade", "Espero poder divertir e melhorar o dia de todos os que tirarem um pouco do seu tempo para participar desse sonho.", 2, 1],
