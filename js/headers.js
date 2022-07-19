f45989c59b2ac3abee7c2ade5e53e9c2a1cb0b21
DaniloAlves <danilobernardino786@gmail.com>
2022-07-18 21:19:16 -0300

    Busca por nick

--- js/headers.js
+++ js/headers.js
@@ -1,21 +1,29 @@
 function header1(){
     header.innerHTML = `
-    <div  id="head" class="bg-realy-dark text-light text-left p-3 pt-4 d-flex flex-wrap" style="text-decoration: none;">
+    <div  id="head" class="justify-content-between bg-realy-dark text-light text-left p-3 pt-4 d-flex flex-wrap" style="text-decoration: none;">
         <a href="index.html" style="text-decoration: none;">
             <h4 class="text-light">< Voltar</h4>
         </a>
-        <h2 class="colorify-text" style="margin-left: 41%;">Follows</h2>
+        <h2 class="colorify-text" style="margin-left: 24%;">Follows</h2>
+        <div class="d-flex" style="max-width: 30%; min-width: 30%;">
+            <input id="txtBusca" type="text" class="input-group" style="border-radius: 5px 0px 0px 5px" placeholder="Buscar por nickname"></input>
+            <button class="btn btn-primary" style="border-radius: 0px 5px 5px 0px" onclick="pesquisar()">Buscar</button>
+        </div>
     </div>
     `
 }
 
 function header2(){
     header.innerHTML = `
-    <div  id="head" class="bg-realy-dark text-light text-left p-3 pt-4 d-flex flex-wrap" style="text-decoration: none;">
+    <div  id="head" class="justify-content-between bg-realy-dark text-light text-left p-3 pt-4 d-flex flex-wrap" style="text-decoration: none;">
         <a href="index.html" style="text-decoration: none;">
             <h4 class="text-light">< Voltar</h4>
         </a>
-        <h2 class="colorify-text" style="margin-left: 41%;">Subs</h2>
+        <h2 class="colorify-text" style="margin-left: 24%;">Subs</h2>
+        <div class="d-flex" style="max-width: 30%; min-width: 30%;">
+            <input id="txtBusca" type="text" class="input-group" style="border-radius: 5px 0px 0px 5px" placeholder="Buscar por nickname"></input>
+            <button class="btn btn-primary" style="border-radius: 0px 5px 5px 0px" onclick="pesquisar()">Buscar</button>
+        </div>
     </div>
     `
 }
