e1f4e25db10c37fb991a3745fe8ad983bd0906e7
DaniloAlves <danilobernardino786@gmail.com>
2022-07-19 10:08:08 -0300

    footer1

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+footer.innerHTML = `
+    <!-- Copyright -->
+    <div class="text-center p-3 text-light">
+      © 2022 Copyright:
+      <a class="text-primary" style="text-decoration: none;" href="https://www.twitch.tv/zegfy">Zegfy</a>
+    </div>
+
+`
